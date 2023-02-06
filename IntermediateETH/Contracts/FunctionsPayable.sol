5e979a32aa6de2f0d3bfbaef0dca319bf6db4e54
prijacash <prijacash@gmail.com>
2023-02-06 13:27:21 -0800

    functions payable

---
+++ IntermediateETH/Contracts/FunctionsPayable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.15; 
+
+contract payableContract {
+
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function deposit () external payable {}
+
+    function getBalance () external view returns (uint) {
+        return address(this).balance;
+    }
+    
+
+}
