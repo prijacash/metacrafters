5dc29bc07b77f2962acb7871c55f5605213befd7
prijacash <prijacash@gmail.com>
2023-02-06 16:56:16 -0800

    error functions

---
+++ IntermediateETH/Contracts/Error.sol
@@ -0,0 +1,6 @@
+// SPDX-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// require
+// revert - revert to initial state before function, saves gas
+// asset - checks for conditions, takes only 1 parameter.
