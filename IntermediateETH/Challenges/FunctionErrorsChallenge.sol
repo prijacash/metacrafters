058c9b02c71e7d5955a04a162a9d66dec74242d2
prijacash <prijacash@gmail.com>
2023-02-07 17:55:06 -0800

    updated

--- IntermediateETH/Challenges/FunctionErrorsChallenge.sol
+++ IntermediateETH/Challenges/FunctionErrorsChallenge.sol
@@ -6,24 +6,19 @@ contract SampleContract {
     uint256 public constant MINIMUM_DEPOSIT = 100;
 
     function depositRequire (uint256 amount) public pure {
-        // - condition before it executes
         require(amount >= MINIMUM_DEPOSIT, "Deposit amount is too low.");
-
     }
 
-    function testRevert (uint256 amount) public pure {
-        // reverts code and will be able to refund any gas left over
-        if (amount <= 100) {
-            revert("Input must be greater than 100");
+    function testRevert(uint256 amount) public pure {
+        if (amount < MINIMUM_DEPOSIT) {
+            revert("Deposit must be equal to or greater than MINIMUM_DEPOSIT");
         }
-
     }
 
     uint public num;
 
     function testAssert() public view {
-        // Assert should only be used to test for internal errors,
-        assert(num == 0);
+        assert(num <= 100, "num must be less than or equal to 100");
     }
     
 
